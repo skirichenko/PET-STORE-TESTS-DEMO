da6c5bc755b0581091ab5cd8fcb9b4472f9d9252
Svitlana Kyrychenko <ss.kirichenko@gmail.com>
2024-09-04 23:51:41 +0200

    introduce types for request and response

---
+++ tests/api/controller/store.controller.ts
@@ -0,0 +1,12 @@
+import { operations } from "../../../.temp/types";
+import { JsonRequest } from "../request";
+
+export class StoreController {
+    async getInventory() {
+        return(
+            await new JsonRequest()
+            .url(`http://localhost/v2/store/inventory`)
+            .send<operations['getInventory']['responses']['200']['schema']>()
+        ).data
+    }
+}
