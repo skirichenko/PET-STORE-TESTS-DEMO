66916f615722ddb7f10a79ab9e327a7f584d63d7
Svitlana Kyrychenko <ss.kirichenko@gmail.com>
2024-10-15 00:54:49 +0200

    schema validation

--- tests/api/controller/pet.controller.ts
+++ tests/api/controller/pet.controller.ts
@@ -2,14 +2,64 @@ import { URLSearchParams } from 'url';
 import { JsonRequest } from '../request';
 import { definitions} from '../../../.temp/types';
 import { operations } from '../../../.temp/types';
+import { validate } from '../validator'
 
 export class PetController {
     async getById(id: number | string) {
-        return(
+        const body = (
             await new JsonRequest()
                 .url(`http://localhost/v2/pet/${id}`)
                 .send<operations['getPetById']['responses']['200']['schema']>()
         ).data
+        const schema = {
+            "$id": "http://example.com/example.json",
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "category": {
+                    "type": "object",
+                    "properties": {
+                        "id": {
+                            "type": "integer"
+                        },
+                        "name": {
+                            "type": "string"
+                        }
+                    }
+                },
+                "name": {
+                    "type": "string"
+                },
+                "photoUrls": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
+                "tags": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "id": {
+                                "type": "integer"
+                            },
+                            "name": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                },
+                "status": {
+                    "type": "string"
+                }
+            }
+        }
+        validate(schema, body)
+
+        return body
     }
 
     async findByStatus(status: string | string[]) {
